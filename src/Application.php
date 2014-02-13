fb7c977409ca8be972f247fa9cb744ef9d03ddb7
Luis Cordova <cordoval@gmail.com>
2014-02-12 23:57:40 -0500

    first commit

---
+++ src/Application.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Charger;
+
+class Application
+{
+    // person
+
+    // t: completes information
+
+    // client
+
+    // t: buys product (recurring)
+
+    // subscriber
+
+    // t: unsubscribe or cancel
+
+    // client
+}
---
+++ tests/Tests/ApplicationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Charger\Tests;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_does_something()
+    {
+        $this->assertTrue(true);
+    }
+}
